004f6a157714c2f504eda951bd02012e386a85d5
ofor-otitodirichukwu <oforpraisetibess@gmail.com>
2026-07-31 05:34:47 -0700

    Fix Flask server indentation and entrypoint

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def get_current_time():
     current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     return f"The current time: {current_time}"
 
-    if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
